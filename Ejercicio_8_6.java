0a2b9d8e7048e5d3ccc207bf39b84a478238b85c
Maylen Haag <maylenhaag23@gmail.com>
2022-03-03 21:59:21 -0300

    second commit

--- Ejercicio_8_6.java
+++ Ejercicio_8_6.java
@@ -1,7 +1,7 @@
-
 package ejercicio_8_6;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -18,65 +18,99 @@ public class Ejercicio_8_6 {
     se necesita que la aplicación cuente con las funciones básicas. Estas las realizaremos en el main. Como, introducir un elemento, modificar su precio,
     eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio.
     Realizar un menú para lograr todas las acciones previamente mencionadas.    
-        */
-        
+         */
+
         Scanner leer = new Scanner(System.in);
-        
-        HashMap<String,Integer> productos = new HashMap();
-        
+
+        HashMap<String, Integer> productos = new HashMap();
+
         System.out.println("Bienvenido a <Tu almacén online>");
         System.out.println("");
-        
+
+        int aux = 0;
+
         String nombreProducto;
         int precioProducto;
-        
-        System.out.println("MENÚ");
-        System.out.println("1. Introducir un producto.");
-        System.out.println("2. Introducir/modificar el precio de un producto.");
-        System.out.println("3. Eliminar un producto");
-        System.out.println("4. Mostrar los productos que tenemos almacenados con sus respectivos precios.");
-        System.out.println("5. Salir.");
-        System.out.println("");
-        System.out.println("Elija la acción a realizar.");
-        int accion = leer.nextInt();
-        
-        switch(accion){
-            case 1 : 
-                System.out.println("Ingrese el nombre del producto:");
-                nombreProducto = leer.next();
-                
-                productos.put(nombreProducto, 0);
-                
-                break;
-                
-            case 2 :    
-                System.out.println("Ingrese el precio del producto:");
-                precioProducto = leer.nextint();
-                
-                productos.put(nombreProducto, precioProducto);
-                
-                break;
-                
-            case 3 :     
-                
-                break;
-            case 4 :    
-                
-                break;
-            case 5 : 
-                
-                break;
-                
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        do {
+
+            System.out.println("MENÚ");
+
+            System.out.println("1. Introducir un producto.");
+
+            System.out.println("2. Introducir/modificar el precio de un producto.");
+
+            System.out.println("3. Eliminar un producto");
+
+            System.out.println("4. Mostrar los productos que tenemos almacenados con sus respectivos precios.");
+
+            System.out.println("5. Salir.");
+
+            System.out.println("");
+
+            System.out.println("Elija la acción a realizar.");
+
+            int accion = leer.nextInt();
+
+            switch (accion) {
+
+                case 1:
+
+                    System.out.println("Ingrese el nombre del producto:");
+
+                    nombreProducto = leer.next();
+
+                    productos.put(nombreProducto, 0);
+
+                    break;
+
+                case 2:
+
+                    System.out.println("Ingrese el nombre del producto del cual desea ingresar/modificar su precio:");
+
+                    nombreProducto = leer.next();
+
+                    System.out.println("Ingrese el precio del producto:");
+
+                    precioProducto = leer.nextInt();
+
+                    productos.put(nombreProducto, precioProducto);
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Ingrese el nombre del producto que desea eliminar:");
+
+                    nombreProducto = leer.next();
+
+                    productos.remove(nombreProducto);
+
+                    break;
+
+                case 4:
+
+                    for (Map.Entry<String, Integer> auxi : productos.entrySet()) {
+
+                        String key = auxi.getKey();
+
+                        Integer val = auxi.getValue();
+
+                        System.out.println("Producto: " + key + " Precio: " + val);
+
+                    }
+
+                    break;
+
+                case 5:
+
+                    aux++;
+
+                    System.out.println("Usted ha salido del programa exitosamente.");
+
+                    break;
+
+            }
+
+        } while (aux == 0);
     }
-    
 }
